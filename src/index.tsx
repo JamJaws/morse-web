146e13e8385ea1e430986d5265ab4245f610d2f2
John Mogensen <jamjaws@gmail.com>
2024-10-10 16:27:09 +0200

    feat: add start button and trigger Tone start on press

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import NotFound from "./NotFound";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
 
 const router = createBrowserRouter([
   {
@@ -30,4 +29,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
